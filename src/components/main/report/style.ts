19d1763d156d2ec8428390a0e3621478e0d8f0c9
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-07-17 11:18:51 +0900

    feat :: Header #7

---
+++ src/components/main/report/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  aspect-ratio: 1/1.9;
+  display: flex;
+  position: relative;
+
+  background: #1c1d28;
+  box-shadow: 0px 0px 50px 30px rgba(0, 0, 0, 0.2);
+
+  margin: 0 auto;
+`;
